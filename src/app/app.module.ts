81e0a83da5293d8ae5a7e4d19fdd8f30220ead31
wilo0510 <wrpachonl@gmail.com>
2019-12-17 22:43:43 -0500

    Pagina sin adicionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { CaracteristicasComponent } from './caracteristicas/caracteristicas.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { DestinosComponent } from './destinos/destinos.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    CaracteristicasComponent,
+    ClientesComponent,
+    DestinosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/caracteristicas/caracteristicas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caracteristicas',
+  templateUrl: './caracteristicas.component.html',
+  styleUrls: ['./caracteristicas.component.css']
+})
+export class CaracteristicasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
